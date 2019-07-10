6e87041207c4b0897719f99c037f47909daeb893
bcrutchleyi3dr <bcrutchley@i3drobotics.com>
2019-07-10 14:15:27 +0100

    added i3dr_pcl_tools submodule

--- i3dr_tools_ros_WS/devel/share/i3dr_stereo_camera/cmake/i3dr_stereo_cameraConfig.cmake
+++ i3dr_tools_ros_WS/devel/share/i3dr_stereo_camera/cmake/i3dr_stereo_cameraConfig.cmake
@@ -162,7 +162,7 @@ foreach(t ${i3dr_stereo_camera_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "roscpp;nodelet;std_msgs;sensor_msgs")
+set(depends "roscpp;nodelet;std_msgs;sensor_msgs;message_runtime")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
